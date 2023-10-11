ffa6d16381510942e5efe06839ba70f86739dd35
minhajadil <minhajuladil@gmail.com>
2023-10-11 16:35:17 +0600

    Final

--- Cinema_hall.py
+++ Cinema_hall.py
@@ -77,17 +77,25 @@ Enter Option : """)
         id = input("Enter ID: ")
 
         if cine.correct_id(id) == True:
-            ro = input('Input Row:')
+          r = input('Enter Rows and Columns:')
+          lst = r.split()
 
-            if cine.rows >= ro and cine.cols >= col:
+          for i in range(0,len(lst)-1,2):
+            
+            ro = int(lst[i])
+            col = int(lst[i+1])
+
+            if cine.rows-1 >= ro and cine.cols-1 >= col:
                 if cine.booked_seat(id, ro, col) == False:
                     print(
                         f'Your seat ({ro},{col}) is successfully booked for Movie id :{id}')
                     cine.book_seats(id, ro, col)
                 else:
-                    print(f'Seat ({ro},{col}) is already booked')
+                    print(f'Seat ({ro},{col}) is Already booked')
             else:
-                print(f'({ro},{col}) is invalid seat')
+                print('Invalid Rows and Columns')
+
+            
         else:
             print("Invalid ID")
 
